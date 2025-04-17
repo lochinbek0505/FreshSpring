e980b2bf521949074b6f5d7c62a5fea1d2cb7079
Lochinbek-desktop <lbek458@gmail.com>
2025-04-17 15:51:10 +0300

    make sette injection and construktor injection

--- src/main/java/org/example/Dev.java
+++ src/main/java/org/example/Dev.java
@@ -4,16 +4,39 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    private    Laptop laptop;
 
+   private int age;
     public Dev(){
 
         System.out.println("Dev constructor");
 
     }
+
+    public Dev(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
     public void build(){
 
         System.out.println("working on Awesome Projects");
-
+        laptop.compile();
     }
 
 }
